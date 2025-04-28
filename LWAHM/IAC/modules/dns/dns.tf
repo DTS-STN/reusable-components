8377698043397560c913f4497c7ae3b4ff8a126c
blai0624 <mblais93@gmail.com>
2025-04-28 09:00:57 -0400

    removed obsolete dns and storage modules

--- LWAHM/IAC/modules/dns/dns.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_dns_zone" "dns" {
-  name                = var.base_domain
-  resource_group_name = var.dns_rg
-}
--- LWAHM/IAC/modules/dns/outputs.tf
+++
@@ -1,6 +0,0 @@
-output dns_name {
-    value = azurerm_dns_zone.dns.name
-}
-output dns_id {
-    value = azurerm_dns_zone.dns.id
-}
