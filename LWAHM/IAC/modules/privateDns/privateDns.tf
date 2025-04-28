22d41ec1b8ad1fdf5e33a5949182896290f52173
blai0624 <mblais93@gmail.com>
2025-04-28 09:18:26 -0400

    added hosting_model variable to code

--- LWAHM/IAC/modules/privateDns/privateDns.tf
+++ LWAHM/IAC/modules/privateDns/privateDns.tf
@@ -10,8 +10,8 @@ resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_esdc_h
   virtual_network_id    = var.esdc_hub_peered_vnet_id
 }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_lwhp_vnet_link" {
-  name                  = "vnet-link-lwhp-${var.environment}"
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_dns_vnet_link" {
+  name                  = "vnet-link-${var.hosting_model}-${var.environment}"
   resource_group_name   = var.private_dns_rg
   private_dns_zone_name = azurerm_private_dns_zone.privatelink_dns.name
   virtual_network_id    = var.lwhp_vnet_id
