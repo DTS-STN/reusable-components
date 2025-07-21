91373bed554ae225e1df9759debccefb9da157b7
blai0624 <mblais93@gmail.com>
2025-07-21 10:30:41 -0400

    added variables for lwhp_vnet_address_space and approved_bastion_address_space, disabled bgp_route_propagation on route table to comply with azure policy

--- LWAHM/IAC/modules/network/network.tf
+++ LWAHM/IAC/modules/network/network.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-${var.platform}-${var.environment}"
   resource_group_name = var.networking_rg_name
   location            = var.location
-  address_space       = ["11.0.0.0/24", "11.0.1.0/24"]
+  address_space       = [var.lwhp_vnet_address_space] #TODO add the following address space when you receive it: var.approved_bastion_address_space
 }
 
 resource "azurerm_virtual_network_peering" "peer_hub_to_platform" {
@@ -26,6 +26,7 @@ resource "azurerm_virtual_network_peering" "peer_platform_to_hub" {
 }
 
 resource "azurerm_route_table" "route_table_perimeter_firewall" {
+  bgp_route_propagation_enabled = false
   name                = "rt-${var.platform}-${var.environment}"
   resource_group_name = var.networking_rg_name
   location            = var.location
