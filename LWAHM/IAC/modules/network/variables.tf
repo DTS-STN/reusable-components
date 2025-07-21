30ac2789da26fe96c88e8111472f9c935eee4b1b
blai0624 <mblais93@gmail.com>
2025-07-21 10:37:47 -0400

    created variables for platformSubnets.tf address prefixes

--- LWAHM/IAC/modules/network/variables.tf
+++ LWAHM/IAC/modules/network/variables.tf
@@ -39,3 +39,12 @@ variable "lwhp_vnet_address_space" {
 variable "approved_bastion_address_space" {
     type = string
 }
+variable "app_service_subnet_cidr" {
+    type = string
+}
+variable "build_agents_subnet_cidr" {
+    type = string
+}
+variable "private_endpoints_subnet_cidr" {
+    type = string
+}
